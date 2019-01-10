71c386b0342f5f112c480788b40c0651e931a603
jevon <m_jevon@163.com>
2019-01-10 17:40:43 +0800

    discard

---
+++ discard/src/main/java/cn/leon/DiscardServerHandler.java
@@ -0,0 +1,31 @@
+package cn.leon;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.ReferenceCountUtil;
+
+
+public class DiscardServerHandler extends ChannelHandlerAdapter { // (1)
+
+    /**
+     * 读取资源
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        // Discard the received data silently.
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            System.out.println(in.toString(io.netty.util.CharsetUtil.US_ASCII));
+        } finally {
+            ReferenceCountUtil.release(msg); // (2)
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
